1848767e6e4628a8ad6b54e68ca3f9c070023909
Ivan <izelehovski@gmail.com>
2021-10-12 04:00:21 +0200

    tooltip and localStorage "database"

--- holCal.js
+++ holCal.js
@@ -2,10 +2,11 @@ var xmlFile = '<reservations><reservation><customer><first_name>hovac</first_nam
 
 /*
     TODO: 
-    - Implement information on clicked dates, or even better span element over all dates taken up by 1 entry
-    - count*n* incrementation in addDate() and delDate()
+    - Implement information on clicked dates, or even better span element over all dates taken up by 1 entry - DONE
+    - count*n* incrementation in addDate() and delDate() - DONE
     - possibility of booking.com admin site scrape?
     - if hosted on website, possible node.js save/load file directly on file system. further discussion needed.
+    - add triangle shit on end date (currently implemented on start date and works good if user inputs date in ascending order)
 */
 
 var lStor = window.localStorage;
@@ -16,7 +17,6 @@ var count = 0;
     fuck javascript and fuck everything about this language. whoever thought of javascript should burn
     loadLocalStorage floor1rooms won't send HTML elements as parameter if there isn't 
 */
-console.log(floor1rooms);
 
 // Global Variables //
 var monthStart = 6;
@@ -110,14 +110,8 @@ var rOverlay = function (room) {
         inputData[1] = oEDate.value;
         inputData[2] = oNames.value;
         inputData[3] = oPrice.value;
-        var apNum = parseInt(room.id.substring(1)) - 1;
-        count = (lStor.getItem("count" + apNum) !== null) ? parseInt(lStor.getItem("count" + apNum)) : 0;
-        lStor.setItem("arrival" + apNum + count, inputData[0]);
-        lStor.setItem("departure" + apNum + count, inputData[1]);
-        lStor.setItem("name" + apNum + count, inputData[2]);
-        lStor.setItem("price" + apNum + count, inputData[3]);
-        count += 1;
-        lStor.setItem("count" + apNum, count);
+        saveToLStor(inputData, room);
+
 
         console.log(lStor, count);
         colorDayBoxes(inputData, room);
@@ -131,6 +125,17 @@ var rOverlay = function (room) {
     }
 }
 
+var saveToLStor = function (inputData, room) {
+    var apNum = parseInt(room.id.substring(1)) - 1;
+    count = (lStor.getItem("count" + apNum) !== null) ? parseInt(lStor.getItem("count" + apNum)) : 0;
+    lStor.setItem("arrival" + apNum + count, inputData[0]);
+    lStor.setItem("departure" + apNum + count, inputData[1]);
+    lStor.setItem("name" + apNum + count, inputData[2]);
+    lStor.setItem("price" + apNum + count, inputData[3]);
+    count += 1;
+    lStor.setItem("count" + apNum, count);
+}
+
 // var download = function(content, fileName, contentType) {
 //     var a = document.createElement("a");
 //     var file = new Blob([JSON.stringify(content)], {type: contentType});
@@ -140,27 +145,56 @@ var rOverlay = function (room) {
 //     URL.revokeObjectURL(a.href);
 // }
 
+var createTooltip = function (day, name, price) {
+    var tSpan = document.createElement("span");
+    tSpan.innerHTML += "ime: " + name + "\n" + "cijena/noć: " + price;
+    tSpan.className = "tooltiptext";
+    day.classList.add("tooltip");
+    day.appendChild(tSpan);
+
+}
+
 var colorDayBoxes = function (iData, room) {
-    console.log(iData, room);
     var tSDate = iData[0].split("-");
     var tEDate = iData[1].split("-");
     if (tSDate[1] == tEDate[1]) {
         var tMonth = room.querySelector("#mjesec_" + parseInt(tSDate[1]));
         var tempDayHolder = [];
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
         for (let i = parseInt(tSDate[2]); i <= parseInt(tEDate[2]); i++) {
             let j = 0;
             tempDayHolder[j] = tMonth.querySelector("#dan_" + i);
             if (i == parseInt(tSDate[2])) {
-                tempDayHolder[j].className = "triangleBStart";
+                var lGradRGB = "rgba(0, 0, 0, 0)";
+                if (tempDayHolder[j].style.backgroundImage) {
+                    var lGradString = tempDayHolder[j].style.backgroundImage.split("rgb")[1];
+                    lGradString = lGradString.slice(0, -5);
+                    //lGradRGB = lGradString.split("rgb")[1];
+                    var rgbColRaw = lGradString.replace(/[^\d,]/g, '').split(',');
+                    lGradRGB = "rgba(" + rgbColRaw[0] + ", " + rgbColRaw[1] + ", " + rgbColRaw[2] + ", 255) 50%";
+                }
+                var eGradArr = [
+                    parseInt(randomColor[0] + randomColor[1], 16),
+                    parseInt(randomColor[2] + randomColor[3], 16),
+                    parseInt(randomColor[4] + randomColor[5], 16),
+                ];
+                var eGradRGB = "rgba(" + eGradArr[0] + ", " + eGradArr[1] + ", " + eGradArr[2] + ", 255) 50%";
+                console.log(eGradRGB);
+                tempDayHolder[j].style.backgroundImage = 'linear-gradient(to bottom right,' + lGradRGB + ', ' + eGradRGB + ')';
+                createTooltip(tempDayHolder[j], iData[2], iData[3]);
             } else if (i == parseInt(tEDate[2])) {
-                tempDayHolder[j].className = "triangleBEnd";
+                tempDayHolder[j].style.backgroundImage = 'linear-gradient(to bottom right, #' + randomColor + ' 50%, rgba(0, 0, 0, 0))';
+
+                createTooltip(tempDayHolder[j], iData[2], iData[3]);
             } else {
-                tempDayHolder[j].style.backgroundColor = "#00FF00";
+                tempDayHolder[j].style.backgroundColor = "#" + randomColor;
+                createTooltip(tempDayHolder[j], iData[2], iData[3]);
             }
             j++
         }
         var tStartDay = tMonth.querySelector("#dan_" + parseInt(tSDate[2]));
         var tEndDay = tMonth.querySelector("#dan_" + parseInt(tEDate[2]));
+        console.log()
 
     } else {
         alert("Mora se unijeti mjesec po mjesec, ne mogu između!");
@@ -214,3 +248,17 @@ var rTitleButtons = function (mTitle, i, room) {
 }
 
 renderDates();
+
+function removeLocalStorage() {
+    if (confirm("Želiš li sigurno izbrisati memoriju?")) {
+        lStor.clear();
+        if (window.localStorage.length == 0) {
+            alert("Svi datumi su obrisani!");
+            location.reload();
+        } else {
+            alert("došlo je do pogreške pri brisanju, nazovi Ivana!");
+        }
+    } else {
+        alert("Nazovi sinka Ivana ako nisi sigurna!");
+    }
+}
